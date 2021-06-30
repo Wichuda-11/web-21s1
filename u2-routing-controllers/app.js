825182f596230ee1c92cf20305c3121faa5066f6
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-06-30 19:49:26 +0700

    U2 Dynamic routes, and their controllers

---
+++ u2-routing-controllers/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { urlencoded, json } = require('body-parser')
+const { getUserByQuery, getUserByPath, getUserByForm, search } = require('./features/userController')
+const { logger } = require('./_services/logger')
+
+const app = express()
+
+// Middlewares
+app.use(urlencoded({ extended: false }))
+app.use(json()) // json = function
+app.use(logger) // logger = middleware
+
+// Routes
+app.get('/users', getUserByQuery)
+app.get('/users/search', search)
+app.get('/users/:username', getUserByPath)
+app.post('/users', getUserByForm)
+
+const PORT = 3000
+app.listen(PORT,
+  () => console.log(`Listening: http://localhost:${PORT}`))
