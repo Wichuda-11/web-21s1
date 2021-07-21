f8b298995f360d87752c1367e222183567f88a75
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-07-21 21:16:35 +0700

    U5 Create book API

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
-const { readBooks, readBook } = require('./features/book-controller')
+const { readBooks, readBook, createBook } = require('./features/book-controller')
 
 const app = express()
 
@@ -12,5 +12,6 @@ app.use(urlencoded({ extended: false }))
 // Routes
 app.get('/books', readBooks)
 app.get('/books/:isbn13', readBook)
+app.post('/books', createBook)
 
 module.exports = { app }
