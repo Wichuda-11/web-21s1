8f48ec89909707ee93b7fcd0a112efae3bac3d7e
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-07-23 19:16:59 +0700

    U5L Covid commit

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
 const { readBooks, readBook, createBook, replaceBook, updateBook, deleteBook } = require('./features/book-controller')
+const { readCovids, readCovid, createCovid, updateCovid, deleteCovid } = require('./features/covid-controller')
 
 const app = express()
 
@@ -16,5 +17,10 @@ app.post('/books', createBook)
 app.put('/books/:isbn13', replaceBook)
 app.patch('/books/:isbn13', updateBook)
 app.delete('/books/:isbn13', deleteBook)
+app.get('/covid', readCovids)
+app.get('/covid/:stateId', readCovid)
+app.post('/covid', createCovid)
+app.patch('/covid/:stateId', updateCovid)
+app.delete('/covid/:stateId', deleteCovid)
 
 module.exports = { app }
