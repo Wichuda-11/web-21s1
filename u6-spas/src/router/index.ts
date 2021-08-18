f04534ed0720fa1c071a39a4e732afd67c22e633
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-08-19 01:39:13 +0700

    U6 update

---
+++ u6-spas/src/router/index.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+import Colors from '../views/Colors.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/colors',
+    name: 'Colors',
+    component: Colors
+  }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
