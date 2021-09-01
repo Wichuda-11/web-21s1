ace77b079f7a8d0fe14a763ee6969f5d84eae47d
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-09-01 23:49:49 +0700

    U8 Add Firebase

--- u8-state/src/store/index.ts
+++ u8-state/src/store/index.ts
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { vuexfireMutations } from 'vuexfire'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
   },
+  getters: {
+  },
   mutations: {
+    ...vuexfireMutations
   },
   actions: {
   },
