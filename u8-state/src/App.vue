cfc4aa2e9239452b2f8b3f400d46d5b08a4ccc55
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-09-03 19:06:04 +0700

    U8L State commit

--- u8-state/src/App.vue
+++ u8-state/src/App.vue
@@ -3,14 +3,20 @@
     <div class="nav">
       <span class="brand">
         <router-link :to="{ name: 'VideoHome' }">DBTube</router-link> /
-        <router-link :to="{ name: 'BooksHome' }">Books</router-link>
+        <router-link :to="{ name: 'BookSearch' }">Books</router-link>
       </span>
       <div>
         <router-link
           :to="{ name: 'VideoPlaylist', params: { slug: 'watch-later' } }"
         >
-          <img src="/images/icons/playlist-play.png" alt="Watch Later" />
-          {{ $store.getters.watchLaterCount }}
+          <img src="/images/icons/playlist-play.png" alt="Watch Later" />{{
+            $store.getters.watchLaterCount
+          }}
+        </router-link>
+        <router-link :to="{ name: 'BookCart' }">
+          <img src="/images/icons/cart-variant.png" alt="Cart" />{{
+            $store.getters.cartCount
+          }}
         </router-link>
       </div>
     </div>
