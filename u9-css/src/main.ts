376c0afdb4a6c293734fa114921d668aa47ad013
JibWichuda <84692823+Wichuda-11@users.noreply.github.com>
2021-09-09 18:39:52 +0700

    U9 Vue Homepage

--- u9-css/src/main.ts
+++ u9-css/src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@mdi/font/css/materialdesignicons.min.css'
-import '/main.scss'
+import './main.scss'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
